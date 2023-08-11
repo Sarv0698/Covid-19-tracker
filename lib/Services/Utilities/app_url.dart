8ace7071c9440bb7853b9b738b4687fcdf38113a
Sarvesh <sachinsalve677@gmail.com>
2023-08-11 23:23:31 +0530

    first commit

---
+++ lib/Services/Utilities/app_url.dart
@@ -0,0 +1,8 @@
+class AppUrl {
+  //BASE URL
+  static const String baseUrl = 'https://disease.sh/v3/covid-19/';
+
+  //FETCH WORLD DATA
+  static const String worldStatsApi = baseUrl + 'all';
+  static const String countriesList = baseUrl + 'countries';
+}
